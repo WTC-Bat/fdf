af5e2c52a499623b5e97681a555b1e9f05371dc9
Matthew Williams <FatBat@Matthews-iMac.local>
2016-09-30 21:15:14 +0200

    aint got shit

---
+++ src/fdf.c
@@ -0,0 +1,15 @@
+#include "fdf.h"
+
+void	print_file()
+{
+
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		ft_putendl_fd("Error: fdf expects a file as a parameter.", 2);
+		return (1);
+	}
+}
